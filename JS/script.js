b451f82fa60792c89cd540c4816003fdf4729205
zywicki-314 <144706181+zywicki-314@users.noreply.github.com>
2023-09-19 13:42:56 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    dots: false,
+    speed: 3000,
+    easing: "ease",
+    autoplay: true,
+    autoplaySpeed: 1000,
+    pausOnFocus: true,
+    pausOnHover: true,
+  });
+});
